191fec8573e958d71e103e9bf79b731ddbf92dd2
Jash Kothari <tech.projectslelo@gmail.com>
2026-04-24 12:17:28 +0530

    env commit

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 // Change this URL in one place when switching between local and production
 // For Vite: use VITE_ prefix to expose to client
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5500";
-
+// const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5500";
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const API_URL = backendUrl;
 export const API_BASE_URL = `${backendUrl}/ecommerce`;
 
